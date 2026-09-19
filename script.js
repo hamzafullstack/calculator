068e83b0772eb67159b342dad28fd7e83cf3ab9e
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-09-19 16:15:45 +0500

    feat: add basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,25 @@
+// Starting with the name of ALLAH. Who is most kind and most merciful.
+
+// Addition function
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+// console.log(add(3, 5));
+
+// subtract function.
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+// console.log(subtract(4, 2));
+
+// multiply function
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+// console.log(multiply(4, 2));
+
+// divide Function.
+function divide(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
+}
+// console.log(divide(5, 2));
