b639b7aba386173a70c772dcb8966db050eccf62
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-09-20 12:43:05 +0500

    feat: implement basic calculator operations

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ let secondNumber = 0;
 
 // Addition function
 function add(firstNum, secondNum) {
-
+    return firstNum + secondNum;
 }
 
 // subtract function.
 function subtract(firstNum, secondNum) {
-    
+    return firstNum - secondNum;
 }
 
 // multiply function
 function multiply(firstNum, secondNum) {
-    
+    return firstNum * secondNum;
 }
 
 
 // divide Function.
 function divide(firstNum, secondNum){
-    
+    return firstNum / secondNum;
 }
 
 // operate function 
